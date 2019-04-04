4dd622398721da5b4af4a5209d9ee33d88a26540
gov <govi218mu@gmail.com>
2019-04-04 18:29:47 -0400

    migrated to jupyter nb

--- data_gen.sh
+++ data_gen.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# get all petitions
-for i in {1..2}
+# Just to see what we're dealing with; actual data gen is done in the petiiton_gen notebook
+for i in {1..5}
 do
-#    echo "curl https://petition.parliament.uk/petitions.json?page=${i}&state=all >> data.json" >> data.json
-    curl https://petition.parliament.uk/petitions.json?page=${i}&state=all >> data.json
-#    sed -i 's/]}/]},/' data.json
+    curl https://petition.parliament.uk/petitions.json?page=$i >> data.json
+    sed -i 's/]}/]}\n/' data.json
 done
-#sed -i 's/]}/]},/' data.json
